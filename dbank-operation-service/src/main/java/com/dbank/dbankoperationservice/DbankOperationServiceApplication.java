53534bb30611d3f0dab93438ae1314def36b29e7
Denis Vasileuski <d.vasileuski@andersenlab.com>
2024-05-22 16:18:45 +0200

    init

---
+++ dbank-operation-service/src/main/java/com/dbank/dbankoperationservice/DbankOperationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dbank.dbankoperationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbankOperationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbankOperationServiceApplication.class, args);
+    }
+
+}
---
+++ dbank-operation-service/src/test/java/com/dbank/dbankoperationservice/DbankOperationServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.dbank.dbankoperationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbankOperationServiceApplicationTests {
+
+}
